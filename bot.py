1c8a3431ec64bf3b399762bf5e8a0280591e3830
semantic-release <semantic-release>
2020-09-25 21:13:21 -0400

    20.57.0
    Automatically generated by python-semantic-release

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ import meetings
 
 # -----------------------------------------------------------------------------
 
-__version__ = "20.56.0"
+__version__ = "20.57.0"
 
 app = web.Application()  # web app for listening to webhooks
 
