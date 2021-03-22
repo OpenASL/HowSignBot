65cf021d97c3c4d3cb3bbae897f71c28d6ed74c7
Steven Loria <sloria1@gmail.com>
2021-03-21 23:53:48 -0400

    feat(practice): add mentions

--- tests/bot/exts/test_practice.py
+++ tests/bot/exts/test_practice.py
@@ -34,12 +34,12 @@ async def mock_worksheet(monkeypatch, db):
         WorksheetMock.get_all_values.return_value = [
             ["docs", "more docs", ""],
             ["Start time", "Host (optional)", "Notes (optional)", ""],
-            ["Friday 5pm EDT", "Steve", "recurring", ""],
-            ["Friday 6pm EDT", "Steve", "paused", "x"],
-            ["Wed 6pm edt", "", "another recurring", ""],
-            ["9/26 2pm PDT 2020", "Steve", "one time", ""],
-            ["Sunday, September 27 02:00 PM EDT 2020", "", "another 1️⃣", ""],
-            ["Nov 26 02:00 PM EDT 2020", "", "Turkey day PRACTICE", ""],
+            ["Friday 5pm EDT", "Steve", "<@!12345>", "recurring", ""],
+            ["Friday 6pm EDT", "Steve", "<@!12345>", "paused", "x"],
+            ["Wed 6pm edt", "", "", "another recurring", ""],
+            ["9/26 2pm PDT 2020", "Steve", "<@!12345>", "one time", ""],
+            ["Sunday, September 27 02:00 PM EDT 2020", "", "", "another 1️⃣", ""],
+            ["Nov 26 02:00 PM EDT 2020", "", "", "Turkey day PRACTICE", ""],
         ]
         mock_get_worksheet.return_value = mock_get_worksheet2.return_value = WorksheetMock
         yield WorksheetMock
@@ -102,7 +102,11 @@ async def test_schedule_no_practices(snapshot, mock_worksheet):
 @pytest.mark.asyncio
 async def test_practice(snapshot, mock_worksheet, start_time, store):
     await practice.practice_impl(
-        guild_id=1234, host="Steve", start_time=start_time, user_id=12345679
+        guild_id=1234,
+        host="Steve",
+        mention="<@!12345>",
+        start_time=start_time,
+        user_id=12345679,
     )
     mock_worksheet.append_row.assert_called_once()
     assert mock_worksheet.append_row.call_args[0][0] == snapshot
@@ -111,7 +115,11 @@ async def test_practice(snapshot, mock_worksheet, start_time, store):
 @pytest.mark.asyncio
 async def test_practice_caches_timezone(mock_worksheet, store):
     await practice.practice_impl(
-        guild_id=1234, host="Steve", start_time="tomorrow 8pm est", user_id=123
+        guild_id=1234,
+        host="Steve",
+        mention="<@!12345>",
+        start_time="tomorrow 8pm est",
+        user_id=123,
     )
     timezone = await store.get_user_timezone(123)
     assert timezone == pytz.timezone("America/New_York")
@@ -121,7 +129,11 @@ async def test_practice_caches_timezone(mock_worksheet, store):
 async def test_practice_errors_if_time_zone_cannot_be_parsed(mock_worksheet, store):
     with pytest.raises(commands.errors.BadArgument, match="Could not parse time zone"):
         await practice.practice_impl(
-            guild_id=1234, host="Steve", start_time="tomorrow 8pm", user_id=321
+            guild_id=1234,
+            host="Steve",
+            mention="<@!12345>",
+            start_time="tomorrow 8pm",
+            user_id=321,
         )
 
 
@@ -129,12 +141,21 @@ async def test_practice_errors_if_time_zone_cannot_be_parsed(mock_worksheet, sto
 async def test_practice_nearby_date(snapshot, mock_worksheet, store):
     with freeze_time("2020-09-25 14:00:00"):
         await practice.practice_impl(
-            guild_id=1234, host="Steve", start_time="10:30am edt", user_id=123
+            guild_id=1234,
+            host="Steve",
+            mention="<@!12345>",
+            start_time="10:30am edt",
+            user_id=123,
         )
 
     mock_worksheet.append_row.assert_called_once()
     appended_row = mock_worksheet.append_row.call_args[0][0]
-    assert appended_row == ("Friday, September 25 10:30 AM EDT 2020", "Steve", "")
+    assert appended_row == (
+        "Friday, September 25 10:30 AM EDT 2020",
+        "Steve",
+        "<@!12345>",
+        "",
+    )
 
 
 @pytest.mark.parametrize(
@@ -153,7 +174,11 @@ async def test_practice_common_mistakes(snapshot, mock_worksheet, start_time):
         commands.errors.CommandError, match="️To schedule a practice, enter a time"
     ):
         await practice.practice_impl(
-            guild_id=1234, host="Steve", start_time=start_time, user_id=123
+            guild_id=1234,
+            host="Steve",
+            mention="<@!12345>",
+            start_time=start_time,
+            user_id=123,
         )
     mock_worksheet.append_row.assert_not_called()
 
@@ -166,6 +191,10 @@ async def test_practice_invalid(mock_worksheet, store):
         match='Could not parse "invalid" into a date or time. Make sure to include "am" or "pm" as well as a timezone',
     ):
         await practice.practice_impl(
-            guild_id=1234, host="Steve", start_time="invalid", user_id=123
+            guild_id=1234,
+            host="Steve",
+            mention="<@!12345>",
+            start_time="invalid",
+            user_id=123,
         )
     mock_worksheet.append_row.assert_not_called()
