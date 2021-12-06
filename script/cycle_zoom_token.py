4c546c9fc80e7e6f2a42f1e416e0c14079300753
Steven Loria <sloria1@gmail.com>
2021-12-05 21:12:34 -0500

    chore: add script for cycling zoom token (#348)

---
+++ script/cycle_zoom_token.py
@@ -0,0 +1,34 @@
+import datetime as dt
+import subprocess
+
+import jwt
+from environs import Env
+
+env = Env()
+env.read_env()
+
+ZOOM_API_KEY = env.str("ZOOM_API_KEY")
+ZOOM_SECRET_KEY = env.str("ZOOM_SECRET_KEY")
+HEROKU_APP = env.str("HEROKU_APP", "howsign")
+
+
+def generate_token():
+    now = dt.datetime.now(tz=dt.timezone.utc)
+    payload = {
+        "aud": None,
+        "iss": ZOOM_API_KEY,
+        "exp": now + dt.timedelta(days=8),
+        "iat": now,
+    }
+    return jwt.encode(payload, ZOOM_SECRET_KEY, algorithm="HS256")
+
+
+def main():
+    token = generate_token()
+    print("==> Setting ZOOM_JWT in Heroku")
+    subprocess.call(("heroku", "config:set", f"ZOOM_JWT={token}", "-a", HEROKU_APP))
+    print("==> Finished.")
+
+
+if __name__ == "__main__":
+    main()
