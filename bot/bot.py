f9444e23a2e1d72c75865fd0f33d9dc8d6720cf7
Steven Loria <sloria1@gmail.com>
2021-12-31 15:57:22 -0500

    fix: attempt to avoid rate limit

--- bot/bot.py
+++ bot/bot.py
@@ -46,6 +46,8 @@ CHANGE_PRESENCE_EVERY = 120  # seconds
 
 
 async def cycle_presence():
+    # Add a sleep before setting presence to avoid getting rate-limited
+    await asyncio.sleep(60)
     for command in itertools.cycle(PRESENCE_COMMANDS):
         activity = disnake.Activity(
             name=command,
