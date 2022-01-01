9d3b144a1f97bbc95e1b816993bcc373cf19f829
Steven Loria <sloria1@gmail.com>
2022-01-01 08:33:52 -0500

    refactor: use gspread.service_account_from_dict

--- bot/utils/gsheets.py
+++ bot/utils/gsheets.py
@@ -1,6 +1,4 @@
 import gspread
-from google.auth.crypt._python_rsa import RSASigner
-from google.oauth2.service_account import Credentials
 
 from bot.settings import GOOGLE_CLIENT_EMAIL
 from bot.settings import GOOGLE_PRIVATE_KEY
@@ -8,14 +6,15 @@ from bot.settings import GOOGLE_PRIVATE_KEY_ID
 from bot.settings import GOOGLE_PROJECT_ID
 from bot.settings import GOOGLE_TOKEN_URI
 
+credentials = {
+    "type": "service_account",
+    "project_id": GOOGLE_PROJECT_ID,
+    "private_key_id": GOOGLE_PRIVATE_KEY_ID,
+    "private_key": GOOGLE_PRIVATE_KEY,
+    "client_email": GOOGLE_CLIENT_EMAIL,
+    "token_uri": GOOGLE_TOKEN_URI,
+}
+
 
 def get_gsheet_client():
-    signer = RSASigner.from_string(key=GOOGLE_PRIVATE_KEY, key_id=GOOGLE_PRIVATE_KEY_ID)
-    credentials = Credentials(
-        signer=signer,
-        service_account_email=GOOGLE_CLIENT_EMAIL,
-        token_uri=GOOGLE_TOKEN_URI,
-        scopes=gspread.auth.DEFAULT_SCOPES,
-        project_id=GOOGLE_PROJECT_ID,
-    )
-    return gspread.authorize(credentials)
+    return gspread.service_account_from_dict(credentials)
